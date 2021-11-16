4f5586bc2ed880a0eac6890682acd56dd8a21be5
Rhaqim <anusiemj@gmail.com>
2021-11-16 03:30:25 +0100

    initial commit for phoneapp

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import propTypes from 'prop-types'
+
+const Header = ({ title }) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+Header.propTypes = {
+    title: propTypes.string.isRequired,
+}
+
+export default Header
